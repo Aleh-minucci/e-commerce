693ebd82b7a1271fda81fb049cd0a0e16acc179b
Alessandra_minucci <alessandraminucci05@gmail.com>
2026-07-06 16:25:36 -0300

    feat(app.ts)Importa o componente produto

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//import { RouterOutlet } from '@angular/router';//remove importação do RouterOutlet por enquanto
+import { Produto } from './components/produto/produto'; //Importa o componente produto
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Produto],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

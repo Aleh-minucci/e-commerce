acd1ed0ac3a44967cbd3dd43bd69bde5a3ab4dc4
Alessandra_minucci <alessandraminucci05@gmail.com>
2026-07-14 15:01:11 -0300

    feat(produto.ts):criação de produtos para a loja virtual

--- src/app/components/produto/produto.ts
+++ src/app/components/produto/produto.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
-
+import { UpperCasePipe, CurrencyPipe} from '@angular/common';
+import { PrecoFormatadoPipe } from '../../pipes/preco-formatado-pipe';
 @Component({
   selector: 'app-produto',
-  imports: [],
+  imports: [UpperCasePipe, PrecoFormatadoPipe],
   templateUrl: './produto.html',
   styleUrl: './produto.css',
 })
 export class Produto {
-  produto = 'Notebook';
-  preco = 2500;
-  mostrarPreco = true;
-  mostrarProduto = false;
+//produto = 'Notebook';
+//preco = 1200.99;
+//mostrarPreco = true;
+//mostrarProduto = true;
+produtos  = [
+  {produto:'Monitor',preco: 1000},
+  {produto:'Teclado',preco: 300},
+  {produto:'Mouse',preco: 250},
+  {produto:'Cadeira',preco: 600},
+  {produto:'Placa de vídeo',preco: 2000}
+];
 }

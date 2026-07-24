c4a2fb998a78e66a93f2fde794c835e7253baf7f
Alessandra_minucci <alessandraminucci05@gmail.com>
2026-07-24 12:57:19 -0300

    feat(app.ts): adiciona login e logout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { usuarioLogado, login, logout } from './core/auth';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink],
-  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('meu-prieiro-app');
+  usuarioLogado = usuarioLogado;
+  login = login;
+  logout = logout;
 }

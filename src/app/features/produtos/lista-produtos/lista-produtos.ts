a82244f433a37781df64d65db16f7248bc1af9a8
Alessandra_minucci <alessandraminucci05@gmail.com>
2026-07-24 23:04:44 -0300

    feat(lista-produtos.ts): Integração do HttpClient com a FakeStoreAPI e Signals

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,62 +1,89 @@
 import { Component, signal, computed, effect } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 import { Produto } from '../produto/produto';
+import { title } from 'process';
 
 @Component({
   selector: 'app-lista-produtos',
+  standalone: true,
   imports: [Produto],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
+
 export class ListaProdutos {
-  produtos = signal([
-    {nome: 'Teclado Gamer', preco:149.99},
-    {nome: 'Mouse Gamer', preco:299.99},
-    {nome: 'Desktop Gamer', preco:1599.99},
-    {nome: 'Monitor Gamer', preco:4999.99},
-    {nome: 'Headset Gamer', preco:699.99}
-  ]);
-  exibirProduto (nome: string){
-    //console.log ('Produto Selecionado:', nome);
-    this.produtoSelecionado.set(nome);
-  }
-  adicionarProduto(){
-    this.produtos.update(listaAtual =>[
-      ...listaAtual, {nome:'Processador Inter Core i5', preco:646}
-    ]);
-  }
-    totalProdutos = computed(() => this.produtos().length);
+produtos = signal<{ nome: string; preco: number }[]>([]);
+produtoSelecionado = signal<string | null>(null);
+carrinho = signal<{ nome: string; preco: number }[]>([]);
+carregando = signal(true);
+totalProdutos = computed(() => this.produtos().length);
+ valorTotal = computed(() => {
+ return this.produtos()
+ .reduce((total, item) => total + item.preco, 0);
+ });
+ quantidadeCarrinho = computed(() => this.carrinho().length);
+ totalCarrinho = computed(() => {
+ return this.carrinho()
+ .reduce((total, item) => total + item.preco, 0);
+ });
 
-    valorTotal = computed(() => { return this.produtos().reduce((total, item) => total + item.preco,0)});
+constructor(private http: HttpClient) {
+ 
+ this.carregarProdutos();
 
-    substituirProdutos (){
-      this.produtos.set([
-        {nome: 'Teclado', preco: 40},
-        {nome: 'Mouse', preco: 10},
-        {nome: 'monitor', preco: 100},
-        {nome: 'Desktop', preco: 500},
-        {nome: 'Headset', preco: 25},
-      ]);
-    }
-constructor(){
-  effect(() => {
-    console.log('Lista de Produtos Alterada:', this.produtos());
-  });
-  effect(() => {
-    console.log('Valor total atualizado', this.valorTotal());
-  });
-  effect(() => {
-    if (typeof document !== 'undefined'){
-      document.title = '${this.totalProdutos()} - MInha Loja';
-        }
-  });
-}
-produtoSelecionado = signal<string |null>(null);
-carrinho = signal<{ nome: string; preco: number}[]>([]);
-adicionarAoCarrinho(produto: { nome: string; preco: number }){
+ effect(() => {
+ console.log('Lista de produtos alterada:', this.produtos());
+ });
+ effect(() => {
+ console.log('Valor total atualizado:', this.valorTotal());
+ });
+effect(() => {
+ if (typeof document !== 'undefined') {
+ document.title = `(${this.totalProdutos()}) Minha Loja`;
+ }
+ });
+ }
+ carregarProdutos() {
+ 
+ this.carregando.set(true);
+
+ this.http.get<{ title: string; price: number }[]>
+ ('https://fakestoreapi.com/products')
+ .subscribe({
+ next: (dados) => {
+
+ const produtosFormatados = dados.map(p => ({
+  nome: p.title,
+ preco: p.price
+ }));
+ this.produtos.set(produtosFormatados);
+ this.carregando.set(false); 
+ },
+ error: (erro) => {
+ console.error('Erro ao carregar produtos:', erro);
+ this.carregando.set(false); 
+ }
+ });
+ }
+exibirProduto(nome: string) {
+ this.produtoSelecionado.set(nome);
+ }
+ adicionarProduto() {
+ this.produtos.update(listaAtual => [
+ ...listaAtual,
+ { nome: 'Teclado', preco: 250 }
+ ]);
+ }
+ substituirProdutos() {
+ this.produtos.set([
+ { nome: 'Produto novo', preco: 999 }
+ ]);
+ }
+ adicionarAoCarrinho(produto: { nome: string; preco: number }) {
   this.carrinho.update(listaAtual => [
-    ...listaAtual,produto])};
-    quantidadeCarrinho = computed(() => this.carrinho().length);
-    totalCarrinho = computed(() => {
-      return this.carrinho().reduce((total, item) => total + item.preco,0);
-    });
-  };
+ ...listaAtual,
+ produto
+ ]);
+ }
+}
+  

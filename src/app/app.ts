2595af0a59a88affd192738fd4d534f3e90d2820
Alessandra_minucci <alessandraminucci05@gmail.com>
2026-08-05 16:15:35 -0300

    refactor: melhora codigo

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { usuarioLogado, login, logout } from './core/auth';
 import { Header } from './shared/layout/header/header';
+import { MatAnchor } from "@angular/material/button";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, Header],
+  imports: [RouterOutlet, RouterLink, Header, MatAnchor],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -13,6 +14,6 @@ export class App {
   protected readonly title = signal('e-commerce');
 
   usuarioLogado = usuarioLogado;
-  login = login;
+  login = login; 
   logout = logout;
 }

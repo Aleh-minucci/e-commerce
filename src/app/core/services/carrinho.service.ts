8cfaf1775a1ae7d21936a28b7f0f5437570dcf45
Alessandra_minucci <alessandraminucci05@gmail.com>
2026-08-05 17:17:21 -0300

    feat: nova pasta

---
+++ src/app/core/services/carrinho.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { signal } from "@angular/core";
+import { computed } from "@angular/core";
+
+export class CarrinhoService {
+    //!EStado global
+    private carrinho = signal<{nome: string; preco: number}[]>([]);
+
+//? seletores
+itens = computed(() => this.carrinho());
+quantidadedeitens = computed(() => this.carrinho().length);
+totalitens = computed(() => 
+    this.carrinho().reduce((total, itens) => total + itens.preco,0)
+);
+
+//todo: acoes
+adicionar(produto: {nome: string; preco: number}){
+    this.carrinho.update(lista =>[ ...lista, produto ]);
+}
+
+limpar() {
+    this.carrinho.set([]);
+}
+}

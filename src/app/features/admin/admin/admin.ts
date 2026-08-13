55a1738b280802570c95841586a0ade9f160b8c7
Alessandra_minucci <alessandraminucci05@gmail.com>
2026-08-13 17:12:14 -0300

    refactor:ajeita bug do codigo

--- src/app/features/admin/admin/admin.ts
+++ src/app/features/admin/admin/admin.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { computed } from '@angular/core';
+import { inject } from '@angular/core';
+import { signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,4 +11,30 @@ import { Component } from '@angular/core';
   templateUrl: './admin.html',
   styleUrl: './admin.css',
 })
-export class Admin {}
+export class Admin {
+
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  // simulação - indicadores 
+  totalProdutosCadastrados = signal(20);
+  pedidosPendentes = signal(3);
+  usuariosCadastrados = signal(8);
+
+  usuarioAtual = this.authService.usurioAtual;
+
+  mensagemPerfil = computed(() => {
+    
+    const usuario = this.usuarioAtual();
+    
+    if (!usuario){
+      return('Nenhum usuario Autenticado!');
+    }
+    return`{Usuario autenticado como: ${usuario.perfil}`;
+  });
+
+  sair(){
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
+  }
+}

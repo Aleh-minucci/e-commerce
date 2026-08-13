c5cc50b450c028ad970274c188298bb9f0de4284
Alessandra_minucci <alessandraminucci05@gmail.com>
2026-08-13 16:38:51 -0300

    feat: definir tipo e lógica para perfil de usuário admin e comum

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -3,9 +3,11 @@ import { Injectable} from "@angular/core";
 import { signal } from "@angular/core";
 import { computed } from "@angular/core";
 
+type PerfilUsuario = 'admin' | 'usuario';
+
 type Usuario = {
     email: string;
-    perfil: 'usuario';
+    perfil: PerfilUsuario;
 } 
 
 @Injectable({
@@ -19,12 +21,13 @@ export class AuthService {
     usurioAtual = computed(() => this.usuario());
     token = computed(() => this.tokenJwt());
     usuarioLogado = computed(() => this.usuario() !== null);
+    admin = computed(() => this.usuario()?.perfil === 'admin');
 
     login (email: string, senha: string): boolean{
         if(!email || !senha){
             return false;
     }
-
+    const perfil:PerfilUsuario = email === 'admin@email.com' ? 'admin' : 'usuario';
     const tokenSimulado = 
 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.' +
 'eyJzdWIiOiJhbHVub0B0ZXN0ZS5jb20iLCJwZXJmaWwiOiJ1c3VhcmlvIn0.' 
@@ -32,7 +35,7 @@ export class AuthService {
 
 this.usuario.set({
     email,
-    perfil: 'usuario',
+    perfil,
 });
 
 this.tokenJwt.set(tokenSimulado)
@@ -46,4 +49,8 @@ logout() {
 obterToken(): string | null{
     return this.tokenJwt();
 }
+obterPerfil(): PerfilUsuario | null {
+    return this.usuario()?.perfil ?? null;
 }
+}
+
